64c8d91869395cae660a9b45d8a9ff4f9bba5404
sheriff <chege2452@gmail.com>
2024-10-08 11:12:16 +0300

    implimented entity relationship

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response,jsonify
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from models import db, Hero, Power, HeroPower
